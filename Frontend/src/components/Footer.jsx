2faeef6bc0fa124d86ab9ceca47124e22ceef4bb
Rohit Deshmukh <deshmukhrohit373@gmail.com>
2026-06-03 13:00:22 +0530

    Update Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer>
             <div>
                 <div className="flex flex-col items-center justify-center bg-sky-700 py-10">
-                    <h1 className="text-4xl pb-4 text-white mb-4">Jetlens</h1>
+                    <h1 className="text-4xl pb-4 text-white mb-4">Aerolens</h1>
                     <a href="https://github.com/irohit373/Jetlens-LSTM-NLP-MERN-Microservices" target="_blank" rel="noopener noreferrer" className="flex items-center text-white mb-4">
                         <FaGithub className="h-6 w-6 mr-2" />
                         Aerolens GitHub Repository
